fefd08f10c4477e382fa40d5148143a4f8f9e615
Pedro Monteiro <pedrosergiomaia@gmail.com>
2022-09-09 23:35:44 -0300

    update

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from "vite";
-// import { ghPages } from "vite-plugin-gh-pages";
-
-export default defineConfig({
-  base: "/Calculator-AppIdeas/",
-//   plugins: [ghPages()],
-});
